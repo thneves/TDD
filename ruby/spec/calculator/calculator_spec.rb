46b836e08f6960a05cafe2bef47d916f8e4dab09
thales-pinner <thales.neves@pinpeople.com.br>
2023-02-14 23:50:11 -0300

    More tsts

--- ruby/spec/calculator/calculator_spec.rb
+++ ruby/spec/calculator/calculator_spec.rb
@@ -2,19 +2,19 @@ require '../lib/calculator.rb'
 
 RSpec.describe Calculator do
 
-  describe 'new' do
-    it 'sum method for 2 method' do
-      calc = Calculator.new
-      result = calc.sum(5, 7)
-
-      expect(result).to eq(12)
-    end
-
-    it 'sum method for 2 method with negative numbers' do
-      calc = Calculator.new
-      result = calc.sum(-5, -7)
-
-      expect(result).to eq(-12)
+  describe 'Calculator' do
+    context '#sum' do
+      it 'has positive numbers' do
+        result = subject.sum(5, 7)
+  
+        expect(result).to eq(12)
+      end
+  
+      it 'has negative numbers' do
+        result = subject.sum(-5, 7)
+  
+        expect(result).to eq(2)
+      end
     end
   end
 end
