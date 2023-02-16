bf21ac0fd29e0aa9ae9b8846900f8346daf2cab0
thales-pinner <thales.neves@pinpeople.com.br>
2023-02-15 23:06:56 -0300

    Add matcher for error handling

--- ruby/spec/calculator/calculator_spec.rb
+++ ruby/spec/calculator/calculator_spec.rb
@@ -16,5 +16,13 @@ RSpec.describe Calculator do
         expect(result).to eq(2)
       end
     end
+
+    context '#division' do
+      it 'will raise error when dividing by 0' do
+        expect{ subject.division(3, 0)}.to raise_error(ZeroDivisionError)
+        expect{ subject.division(3, 0)}.to raise_error("divided by 0")
+        expect{ subject.division(3, 0)}.to raise_error(ZeroDivisionError, "divided by 0")
+      end
+    end
   end
 end
